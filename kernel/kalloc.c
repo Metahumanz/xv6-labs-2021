a3d798fe825a3171b30fbc71d27f362d61069532
Metahumanz <Metahumanz@outlook.com>
2026-07-16 17:53:34 +0800

    syscall: 实现sysinfo系统调用

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,19 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64
+freemem(void)
+{
+  struct run *r;
+  uint64 freebytes = 0;
+
+  acquire(&kmem.lock);
+
+  for(r = kmem.freelist; r != 0; r = r->next)
+    freebytes += PGSIZE;
+
+  release(&kmem.lock);
+
+  return freebytes;
+}
